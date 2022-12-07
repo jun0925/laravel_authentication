2178ca8a996eaa59476d0575ec00631cea17b811
YeongJun Kim <yeongjun0925@gmail.com>
2022-12-07 11:57:10 +0900

    AuthCheck middleware

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
                         <div class="alert alert-success">{{ Session::get('success') }}</div>
                     @endif
                     @if (Session::has('fail'))
-                        <div class="alert alert-danger">{{ Session::get('dail') }}</div>
+                        <div class="alert alert-danger">{{ Session::get('fail') }}</div>
                     @endif
                     @csrf
                     <div class="form-group">
